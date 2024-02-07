25fedef70f2570fbdc65167e55650b2a9271682a
unknown <ridwanlasisi3@gmail.com>
2024-02-07 16:50:25 +0100

    commit

--- src/Pages/DriverDetail/Profile/ProfileAd.jsx
+++ src/Pages/DriverDetail/Profile/ProfileAd.jsx
@@ -132,9 +132,7 @@ const ProfileAd = () => {
             </div>
           </div>
           <div style={{ display: "flex", justifyContent: "end" }}>
-            {/* <button className="authorize" disabled style={{ opacity: "0.8" }}>
-              Authorize
-            </button> */}
+           
           </div>
         </form>
       </div>
